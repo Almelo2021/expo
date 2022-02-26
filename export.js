35074a62c5d34953a3b356d287a27a792ee3e037
Almelo2021 <79860581+Almelo2021@users.noreply.github.com>
2022-02-26 17:13:42 +0100

    Create export.js

---
+++ export.js
@@ -0,0 +1,10 @@
+async function x(){
+var y = await exportV();
+var z = "https://script.google.com/macros/s/AKfycbz2LbmG-76qSukYkmXYYWPk1ytkaw3h1XROtobqUMgv2GJuhXz9nTnhnqOmJn7Qh3WhuQ/exec?blob="+y;
+console.log(z);
+await fetch(z);}
+
+async function exportV() {
+ return fetch("https://reveal.clearbit.com/v1/companies/reveal?authorization=pk_a7bb4cb2fcda3017eb148eeffe8438d7&callback=revealCallback")
+ .then(response => response.text())
+}
