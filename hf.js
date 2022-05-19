58b1fd0eaf812741d4753b22fc9cba7bf41dd622
Almelo2021 <79860581+Almelo2021@users.noreply.github.com>
2022-05-19 23:07:08 +0200

    Update hf.js

--- hf.js
+++ hf.js
@@ -1,6 +1,6 @@
 function x(){
 console.log('henniefinke');
-  location.replace("https://www.w3schools.com/css/mystyle.css");
+location.replace("https://www.w3schools.com/css/mystyle.css");
 }
 
 x();
